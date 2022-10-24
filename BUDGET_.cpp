22d28a2166d4428675477e49545a73a39bca77e4
suchijain30 <116373429+suchijain30@users.noreply.github.com>
2022-10-24 11:28:46 +0530

    Add files via upload

---
+++ BUDGET_.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+       int x , y ;
+       cin>>x>>y ;
+       x>y ;
+      if (x>=y*30)
+      {
+          cout<<"yes"<<endl;
+      }
+      else 
+      {
+          cout<<"no"<<endl;
+      }
+      
+    }
+	return 0;
+}
