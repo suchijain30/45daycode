3561682b8c7928e2ef06457743685442b6efc8a5
suchijain30 <116373429+suchijain30@users.noreply.github.com>
2022-10-27 22:26:19 +0530

    Add files via upload

---
+++ CREDS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+        int X , Y , Z ;
+        cin>>X>>Y>>Z ;
+        cout<<(X*4+Y*2)<<endl;
+       
+      
+    }
+	return 0;
+}
