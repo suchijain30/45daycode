6005d450099ad70069b56e86328e2a1cbd3a65f8
suchijain30 <116373429+suchijain30@users.noreply.github.com>
2022-10-28 10:09:12 +0530

    Add files via upload

---
+++ HOOPS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+       int N  ;
+       N%2==1 ;
+       cin>>N ;  
+       cout<<(N+1)/2<<endl;
+    }
+	return 0;
+}
