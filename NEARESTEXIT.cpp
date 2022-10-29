515b9cb6b0256e114da8128200c1ded449984e77
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-10-29 23:27:54 +0530

    Add files via upload

---
+++ NEARESTEXIT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+       int x ;
+       cin>>x;
+       if ( x>50)
+       {
+           cout<<"right"<<endl;
+       }
+      else
+      {
+          cout<<"left"<<endl;
+      }
+    }
+	return 0;
+}
