105ec330d1d239e4d7011ac804d294ef987b6b66
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-06 22:49:57 +0530

    Add files via upload

---
+++ FOOTCUP.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+        int X, Y ;
+        cin>>X>>Y ;
+        if(X==Y && X>0)
+        cout<<"yes"<<endl;
+        else 
+        cout<<"no"<<endl;
+       
+      
+    }
+	return 0;
+}
