3d01a2d1bae272a217f183a2366e077c10eacc19
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-07 20:49:12 +0530

    Add files via upload

---
+++ FIT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+       int X;
+       cin>>X;
+       cout<<X*10<<endl;
+      
+    }
+	return 0;
+}
