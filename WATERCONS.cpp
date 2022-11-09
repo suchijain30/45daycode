0341e0e6e8f8a4a056c4f971460752d3491636b3
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-09 21:14:13 +0530

    Add files via upload

---
+++ WATERCONS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+      int X ;
+      cin>>X ;
+      if(X>=2000)
+      cout<<"yes"<<endl;
+      else
+      cout<<"no"<<endl;
+      
+    }
+	return 0;
+}
