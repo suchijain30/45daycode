f990e14c8eef922691d582cc503a8a82fe58f8ed
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-10 21:02:40 +0530

    Add files via upload

---
+++ CHESSTIME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+        int N ;
+        cin>>N ;
+        cout<<(N*60)/20<<endl;
+    }
+	return 0;
+}
