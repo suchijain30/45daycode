67e8656b0a51cd4671fb5b9950460cfe1331c79b
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-13 20:01:08 +0530

    Add files via upload

---
+++ VOWELTB.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+       char C ;
+       cin>>C;
+       if(C=='A'|| C=='E'|| C=='I'||C=='O'|| C=='U')
+       cout<<"vowel"<<endl;
+       else
+       cout<<"Consonant"<<endl;
+	return 0;
+}
