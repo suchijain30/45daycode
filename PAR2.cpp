eafcac1f68317c10b70f369a5b8cbd01f9f6fd7d
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-18 12:50:16 +0530

    Add files via upload

---
+++ PAR2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+       int  N ;
+       cin>>N ;
+       if(N%2==0)
+       cout<<"Yes"<<endl;
+       else 
+       cout<<"No"<<endl;
+      
+    }
+	return 0;
+}
