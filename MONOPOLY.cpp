c3847dc03b3b6e122e80389553aaa77c5a1f0157
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-24 21:22:31 +0530

    Add files via upload

---
+++ MONOPOLY.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+       int r1, r2 , r3 ;
+       cin>>r1>>r2>>r3;
+       if(r1+r2<r3 ||r2+r3<r1 ||r1+r3<r2)
+       cout<<"Yes"<<endl;
+       else 
+       cout<<"No"<<endl;
+    }
+	return 0;
+}
