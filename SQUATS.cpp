63247c3ba207f7bfd205faa293ccb041f771f8d5
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-11-25 23:34:26 +0530

    Add files via upload

---
+++ SQUATS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+      int x ;
+      cin>>x ;
+      std::cout << x*15 << std::endl;
+      
+    }
+	return 0;
+}
