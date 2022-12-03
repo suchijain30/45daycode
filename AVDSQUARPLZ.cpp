a4d457335864485ce32a34053babc22d3047ee2c
suchi jain <116373429+suchijain30@users.noreply.github.com>
2022-12-03 11:59:32 +0530

    Add files via upload

---
+++ AVDSQUARPLZ.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t ;
+    cin>> t ;
+    while(t--)
+    {
+      int N ;
+      cin>>N;
+      for(int i=1; i<=N ; i++)
+      cout<<i<<endl;
+      cout<<" "<<endl;
+      
+    }
+	return 0;
+}
